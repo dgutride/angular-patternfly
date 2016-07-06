c48d2ea85b49f2d005b1fc5671d95652515711c2
Dana Gutride <dgutride@redhat.com>
2016-07-06 14:02:01 -0400

    Run script every time (for now)

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o nounset
 
-if [ "$TRAVIS_BRANCH" != "master" ]
+if [ "$TRAVIS_BRANCH" == "master" ]
 then
   echo "This commit was made against the $TRAVIS_BRANCH and not the master! Do not deploy!"
   exit 0
@@ -16,7 +16,7 @@ git init
 git config user.name "Dana Gutride"  #get from commit 
 git config user.email "dgutride@redhat.com" # get from original commit
 
-git add .
+#git add .
 
 git remote add upstream "https://$GH_TOKEN@github.com/dgutride/angular-patternfly.git"
 git fetch upstream
@@ -26,6 +26,6 @@ git fetch upstream
 
 #touch .
 
-git add -A .
+#git add -A .
 git commit -m "rebuild pages at ${rev}"
-git push -q origin test_branch
+git push origin test_branch
